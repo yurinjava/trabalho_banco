36ff136ed874f0a43f94fc8eabe035628147169e
yurokkj <yurisouzact@gmail.com>
2022-06-16 15:54:49 -0300

    switch complementado

--- src/trabalho/Banco.java
+++ src/trabalho/Banco.java
@@ -17,11 +17,11 @@ public class Banco {
 		
 		System.out.println("------------------------------------------------------");
 		
-		double crediCorrente, crediPoupanca;
-		String  test="y";
+		double opCorrente, opPoupanca;
+		String  test="y",nula;
 	
 		
-		while("y".equals(test));{
+		while("y".equals(test)){
 			System.out.println("Qual opera�ao deseja realizar?");
 			System.out.println("1-Creditar");
 			System.out.println("2-Debitar");
@@ -38,18 +38,53 @@ public class Banco {
 						int resp1 = sc.nextInt();
 						if(resp1==1) {
 							System.out.println("Qual o valor deseja creditar na conta "+corrente+" ?");
-							crediCorrente = sc.nextDouble();
-							saldoCorrente+=crediCorrente;
+							opCorrente = sc.nextDouble();
+							saldoCorrente+=opCorrente;
 							System.out.println("o saldo atual da conta "+corrente+" e "+saldoCorrente);
 							
 							System.out.println("deseja realizar alguma outra operecao? y/n ");
+							nula=sc.nextLine();
 							 test = sc.nextLine();
 							
+						}else if(resp1==2) {
+							System.out.println("Qual valor deseja creditar na conta "+poupanca+" ?");
+							opPoupanca = sc.nextDouble();
+							saldoPoupanca += opPoupanca;
+							System.out.println("o saldo atual da conta "+poupanca+" e "+saldoPoupanca);
+							
+							System.out.println("deseja realizar alguma outra operecao? y/n ");
+							nula=sc.nextLine();
+							 test = sc.nextLine();
+						}else {
+							System.out.println("Erro! opcao invalida.");
 						}
 						break;
 					}
 					case 2:{
-						System.out.println("debitar");
+						System.out.println("Em qual conta deseja debitar? ");
+						System.out.println("1-Corrente");
+						System.out.println("2-Poupanca");
+						int resp2=sc.nextInt();
+						if(resp2==1) {
+							System.out.println("Qual valor deseja debitar da conta "+corrente+" ?");
+							opCorrente=sc.nextInt();
+							saldoCorrente -=opCorrente;
+							System.out.println("O valor atual da conta "+corrente+" e de "+saldoCorrente);
+							
+							System.out.println("deseja realizar alguma outra operecao? y/n ");
+							nula=sc.nextLine();
+							 test = sc.nextLine();
+						}else if(resp2==2) {
+							System.out.println("Qual valor deseja debitar da conta "+poupanca+" ?");
+							opPoupanca=sc.nextInt();
+							saldoPoupanca -=opPoupanca;
+							System.out.println("O valor atual da conta "+poupanca+" e de "+saldoPoupanca);
+							
+							System.out.println("deseja realizar alguma outra operecao? y/n ");
+							nula=sc.nextLine();
+							 test = sc.nextLine();
+						}
+						
 						break;
 					}
 					case 3:{
@@ -57,21 +92,23 @@ public class Banco {
 						break;
 					}
 					case 4:{
+						System.out.println("------------------------------------------");
 						System.out.println("seu saldo da conta corrente � "+saldoCorrente);
 						System.out.println("seu saldo da conta poupanca � "+saldoPoupanca);
+						System.out.println("------------------------------------------");
+						System.out.println();
+						System.out.println("deseja realizar alguma outra operecao? y/n ");
+						nula=sc.nextLine();
+						 test = sc.nextLine();
 						break;
 					}
 					case 5:{
-						System.out.println("Encerrando aplicacao ");
 						test="n";
 						break;
 					}
-				}
-				
-
+				}		
 		}
-		
+		System.out.println("Encerrando aplicacao ");
 		sc.close();
 	}
-
 }
